fd24700e98e8717cfa634c622d5ebdcbb1f51fb2
Aditya Ghatiya <91601167+Adityaghatiya@users.noreply.github.com>
2023-10-13 17:03:16 +0530

    solution.java

---
+++ CountSumInArray/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    int getPairsCount(int[] arr, int n, int k) {
+        // code here
+
+        HashMap<Integer,Integer>map=new HashMap<>();
+        
+        HashMap<Integer, Integer> m = new HashMap<>();
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (m.containsKey(k - arr[i])) {
+                count += m.get(k - arr[i]);
+            }
+            if (m.containsKey(arr[i])) {
+                m.put(arr[i], m.get(arr[i]) + 1);
+            }
+            else {
+                m.put(arr[i], 1);
+            }
+        }
+        return count;
+    }
+}
