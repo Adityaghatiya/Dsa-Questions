66fc6bcb7ad0dea39cf9dd70d6130b4289b32897
Aditya Ghatiya <91601167+Adityaghatiya@users.noreply.github.com>
2024-03-10 17:25:22 +0530

    Create Iterative Solution.java

---
+++ 24. Swap Nodes in Pairs/Iterative Solution.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy=new ListNode();
+        dummy.next=head;
+        ListNode current=dummy;
+
+        while(current.next!=null && current.next.next!=null)
+        {
+            ListNode first=current.next;
+            ListNode second=current.next.next;
+
+            // swap
+            first.next=second.next;
+            second.next=first;
+            current.next=second;
+
+            current=current.next.next;
+        }
+        return dummy.next;
+    }
+}
