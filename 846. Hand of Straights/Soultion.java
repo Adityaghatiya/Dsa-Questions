9f598f16fbc84f64b3d721360681a6f3f85e3139
Aditya Ghatiya <91601167+Adityaghatiya@users.noreply.github.com>
2024-06-06 15:57:14 +0530

    Create Soultion.java

---
+++ 846. Hand of Straights/Soultion.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        int n=hand.length;
+
+        if(n%groupSize!=0)
+        {
+            return false;
+        }
+        TreeMap<Integer,Integer>map=new TreeMap<>();
+
+        for(int i=0;i<n;i++)
+        {
+            map.put(hand[i],map.getOrDefault(hand[i],0)+1);
+        }
+
+        while(!map.isEmpty())
+        {
+            int curr=map.firstKey();
+
+            for(int i=0;i<groupSize;i++)
+            {
+                if(!map.containsKey(curr+i))
+                {
+                    return false;
+                }
+
+                if(map.get(curr+i)==1)
+                {
+                    map.remove(curr+i);
+                }
+                else{
+                    map.put(curr+i,map.get(curr+i)-1);
+                }
+            }
+        }
+
+        return true;
+    }
+}
